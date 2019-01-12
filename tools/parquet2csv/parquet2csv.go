fb68d7ea8191d161d533be5b9de0a5004e30ba78
Bala.FA <bala.gluster@gmail.com>
2019-01-12 07:55:17 +0530

    Fix zero read at EOF in readByteArrays().

--- tools/parquet2csv/parquet2csv.go
+++ tools/parquet2csv/parquet2csv.go
@@ -76,7 +76,7 @@ func main() {
 		csvFilename = strings.TrimSuffix(name, ext) + ".csv"
 	}
 
-	columns := set.CreateStringSet(os.Args[1:]...)
+	columns := set.CreateStringSet(os.Args[2:]...)
 	if len(columns) == 0 {
 		columns = nil
 	}
