ee78fca07339390f9c3b8a1d5bd2e24bac7a7d8d
Bala FA <bala.gluster@gmail.com>
2019-06-06 22:39:30 +0530

    add JSON record support in writer (#17)
    add encoding support

--- schema/tree.go
+++ schema/tree.go
@@ -23,32 +23,19 @@ import (
 	"github.com/minio/parquet-go/gen-go/parquet"
 )
 
-func setMaxDefinitionLevel(schemaMap map[string]*Element, maxDL int) {
+func updateMaxDLRL(schemaMap map[string]*Element, maxDL, maxRL int64) {
 	for _, element := range schemaMap {
 		element.MaxDefinitionLevel = maxDL
-		switch *element.RepetitionType {
-		case parquet.FieldRepetitionType_REQUIRED:
-		case parquet.FieldRepetitionType_OPTIONAL, parquet.FieldRepetitionType_REPEATED:
-			element.MaxDefinitionLevel++
-		}
-
-		if element.Children != nil {
-			setMaxDefinitionLevel(element.Children.schemaMap, element.MaxDefinitionLevel)
-		}
-	}
-}
-
-func setMaxRepetitionLevel(schemaMap map[string]*Element, maxRL int) {
-	for _, element := range schemaMap {
 		element.MaxRepetitionLevel = maxRL
-		switch *element.RepetitionType {
-		case parquet.FieldRepetitionType_REQUIRED, parquet.FieldRepetitionType_OPTIONAL:
-		case parquet.FieldRepetitionType_REPEATED:
-			element.MaxRepetitionLevel++
+		if *element.RepetitionType != parquet.FieldRepetitionType_REQUIRED {
+			element.MaxDefinitionLevel++
+			if *element.RepetitionType == parquet.FieldRepetitionType_REPEATED {
+				element.MaxRepetitionLevel++
+			}
 		}
 
 		if element.Children != nil {
-			setMaxRepetitionLevel(element.Children.schemaMap, element.MaxRepetitionLevel)
+			updateMaxDLRL(element.Children.schemaMap, element.MaxDefinitionLevel, element.MaxRepetitionLevel)
 		}
 	}
 }
@@ -60,67 +47,150 @@ func toParquetSchema(tree *Tree, treePrefix string, schemaPrefix string, schemaL
 			pathInTree = treePrefix + "." + name
 		}
 
+		if element.Type == nil && element.ConvertedType == nil && element.Children == nil {
+			err = fmt.Errorf("%v: group element must have children", pathInTree)
+			return false
+		}
+
 		if element.ConvertedType != nil {
 			switch *element.ConvertedType {
 			case parquet.ConvertedType_LIST:
+				// Supported structure.
+				// <REQUIRED|OPTIONAL> group <name> (LIST) {
+				//   REPEATED group list {
+				//     <REQUIRED|OPTIONAL> <element-type> element;
+				//   }
+				// }
+
 				if element.Type != nil {
-					err = fmt.Errorf("field %v of Type must be nil for LIST ConvertedType", pathInTree)
+					err = fmt.Errorf("%v: type must be nil for LIST ConvertedType", pathInTree)
 					return false
 				}
 
 				if element.Children == nil || element.Children.Length() != 1 {
-					err = fmt.Errorf("field %v of Children must have one element only for LIST ConvertedType", pathInTree)
+					err = fmt.Errorf("%v: children must have one element only for LIST ConvertedType", pathInTree)
 					return false
 				}
 
 				listElement, ok := element.Children.Get("list")
 				if !ok {
-					err = fmt.Errorf("field %v of Children must have 'list' element for LIST ConvertedType", pathInTree)
+					err = fmt.Errorf("%v: missing group element 'list' for LIST ConvertedType", pathInTree)
+					return false
+				}
+
+				if listElement.Name != "list" {
+					err = fmt.Errorf("%v.list: name must be 'list'", pathInTree)
 					return false
 				}
 
 				if *listElement.RepetitionType != parquet.FieldRepetitionType_REPEATED {
-					err = fmt.Errorf("repetition type of %v.list element must be REPEATED", pathInTree)
+					err = fmt.Errorf("%v.list: repetition type must be REPEATED type", pathInTree)
+					return false
+				}
+
+				if listElement.Type != nil || listElement.ConvertedType != nil {
+					err = fmt.Errorf("%v.list: type and converted type must be nil", pathInTree)
+					return false
+				}
+
+				if listElement.Children == nil || listElement.Children.Length() != 1 {
+					err = fmt.Errorf("%v.list.element: not found", pathInTree)
+					return false
+				}
+
+				valueElement, ok := listElement.Children.Get("element")
+				if !ok {
+					err = fmt.Errorf("%v.list.element: not found", pathInTree)
+					return false
+				}
+
+				if valueElement.Name != "element" {
+					err = fmt.Errorf("%v.list.element: name must be 'element'", pathInTree)
 					return false
 				}
 
 			case parquet.ConvertedType_MAP:
+				// Supported structure:
+				// <REQUIRED|OPTIONAL> group <name> (MAP) {
+				//   REPEATED group key_value {
+				//     REQUIRED <key-type> key;
+				//     <REQUIRED|OPTIONAL> <value-type> value;
+				//   }
+				// }
+
 				if element.Type != nil {
-					err = fmt.Errorf("field %v of Type must be nil for MAP ConvertedType", pathInTree)
+					err = fmt.Errorf("%v: type must be nil for MAP ConvertedType", pathInTree)
 					return false
 				}
 
-				if *element.RepetitionType != parquet.FieldRepetitionType_REPEATED {
-					err = fmt.Errorf("repetition type of %v element must be REPEATED", pathInTree)
+				if element.Children == nil || element.Children.Length() != 1 {
+					err = fmt.Errorf("%v: children must have one element only for MAP ConvertedType", pathInTree)
 					return false
 				}
 
-				if element.Children == nil || element.Children.Length() != 2 {
-					err = fmt.Errorf("field %v of Children must have two elements only for MAP ConvertedType", pathInTree)
-					return false
-				}
-
-				keyElement, ok := element.Children.Get("key")
+				keyValueElement, ok := element.Children.Get("key_value")
 				if !ok {
-					err = fmt.Errorf("field %v of Children must have 'key' element for MAP ConvertedType", pathInTree)
+					err = fmt.Errorf("%v: missing group element 'key_value' for MAP ConvertedType", pathInTree)
+					return false
+				}
+
+				if keyValueElement.Name != "key_value" {
+					err = fmt.Errorf("%v.key_value: name must be 'key_value'", pathInTree)
+					return false
+				}
+
+				if *keyValueElement.RepetitionType != parquet.FieldRepetitionType_REPEATED {
+					err = fmt.Errorf("%v.key_value: repetition type must be REPEATED type", pathInTree)
+					return false
+				}
+
+				if keyValueElement.Children == nil || keyValueElement.Children.Length() < 1 || keyValueElement.Children.Length() > 2 {
+					err = fmt.Errorf("%v.key_value: children must have 'key' and optionally 'value' elements for MAP ConvertedType", pathInTree)
+					return false
+				}
+
+				keyElement, ok := keyValueElement.Children.Get("key")
+				if !ok {
+					err = fmt.Errorf("%v.key_value: missing 'key' element for MAP ConvertedType", pathInTree)
+					return false
+				}
+
+				if keyElement.Name != "key" {
+					err = fmt.Errorf("%v.key_value.key: name must be 'key'", pathInTree)
 					return false
 				}
 
 				if *keyElement.RepetitionType != parquet.FieldRepetitionType_REQUIRED {
-					err = fmt.Errorf("repetition type of %v.key element must be REQUIRED", pathInTree)
+					err = fmt.Errorf("%v.key_value: repetition type must be REQUIRED type", pathInTree)
 					return false
 				}
 
-				if _, ok = element.Children.Get("value"); !ok {
-					err = fmt.Errorf("field %v of Children must have 'value' element for MAP ConvertedType", pathInTree)
+				if keyValueElement.Children.Length() == 2 {
+					valueElement, ok := keyValueElement.Children.Get("value")
+					if !ok {
+						err = fmt.Errorf("%v.key_value: second element must be 'value' element for MAP ConvertedType", pathInTree)
+						return false
+					}
+
+					if valueElement.Name != "value" {
+						err = fmt.Errorf("%v.key_value.value: name must be 'value'", pathInTree)
+						return false
+					}
+				}
+
+			case parquet.ConvertedType_UTF8, parquet.ConvertedType_UINT_8, parquet.ConvertedType_UINT_16:
+				fallthrough
+			case parquet.ConvertedType_UINT_32, parquet.ConvertedType_UINT_64, parquet.ConvertedType_INT_8:
+				fallthrough
+			case parquet.ConvertedType_INT_16, parquet.ConvertedType_INT_32, parquet.ConvertedType_INT_64:
+				if element.Type == nil {
+					err = fmt.Errorf("%v: ConvertedType %v must have Type value", pathInTree, element.ConvertedType)
 					return false
 				}
 
 			default:
-				if element.Type == nil {
-					err = fmt.Errorf("field %v of ConvertedType %v must have Type value", pathInTree, element.ConvertedType)
-					return false
-				}
+				err = fmt.Errorf("%v: unsupported ConvertedType %v", pathInTree, element.ConvertedType)
+				return false
 			}
 		}
 
@@ -138,12 +208,9 @@ func toParquetSchema(tree *Tree, treePrefix string, schemaPrefix string, schemaL
 		if element.Children != nil {
 			element.numChildren = int32(element.Children.Length())
 			err = toParquetSchema(element.Children, element.PathInTree, element.PathInSchema, schemaList, valueElements)
-			if err != nil {
-				return false
-			}
 		}
 
-		return true
+		return (err == nil)
 	})
 
 	return err
@@ -153,8 +220,10 @@ func toParquetSchema(tree *Tree, treePrefix string, schemaPrefix string, schemaL
 type Tree struct {
 	schemaMap map[string]*Element
 	keys      []string
+	readOnly  bool
 }
 
+// String - stringify this tree.
 func (tree *Tree) String() string {
 	var s []string
 	tree.Range(func(name string, element *Element) bool {
@@ -191,8 +260,37 @@ func (tree *Tree) travel(pathSegments []string) (pathSegmentIndex int, pathSegme
 	return
 }
 
+// ReadOnly - returns whether this tree is read only or not.
+func (tree *Tree) ReadOnly() bool {
+	return tree.readOnly
+}
+
+// Get - returns the element stored for name.
+func (tree *Tree) Get(name string) (element *Element, ok bool) {
+	pathSegments := strings.Split(name, ".")
+	for _, pathSegment := range pathSegments {
+		if tree == nil {
+			element = nil
+			ok = false
+			break
+		}
+
+		if element, ok = tree.schemaMap[pathSegment]; !ok {
+			break
+		}
+
+		tree = element.Children
+	}
+
+	return element, ok
+}
+
 // Set - adds or sets element to name.
 func (tree *Tree) Set(name string, element *Element) error {
+	if tree.readOnly {
+		return fmt.Errorf("read only tree")
+	}
+
 	pathSegments := strings.Split(name, ".")
 	if err := validataPathSegments(pathSegments); err != nil {
 		return err
@@ -225,28 +323,12 @@ func (tree *Tree) Set(name string, element *Element) error {
 	return nil
 }
 
-// Get - returns the element stored for name.
-func (tree *Tree) Get(name string) (element *Element, ok bool) {
-	pathSegments := strings.Split(name, ".")
-	for _, pathSegment := range pathSegments {
-		if tree == nil {
-			element = nil
-			ok = false
-			break
-		}
-
-		if element, ok = tree.schemaMap[pathSegment]; !ok {
-			break
-		}
-
-		tree = element.Children
-	}
-
-	return element, ok
-}
-
 // Delete - deletes name and its element.
 func (tree *Tree) Delete(name string) {
+	if tree.readOnly {
+		panic(fmt.Errorf("read only tree"))
+	}
+
 	pathSegments := strings.Split(name, ".")
 
 	_, pathSegment, _, parentTree, found := tree.travel(pathSegments)
@@ -275,14 +357,18 @@ func (tree *Tree) Range(f func(name string, element *Element) bool) {
 
 // ToParquetSchema - returns list of parquet SchemaElement and list of elements those stores values.
 func (tree *Tree) ToParquetSchema() (schemaList []*parquet.SchemaElement, valueElements []*Element, err error) {
-	setMaxDefinitionLevel(tree.schemaMap, 0)
-	setMaxRepetitionLevel(tree.schemaMap, 0)
+	if tree.readOnly {
+		return nil, nil, fmt.Errorf("read only tree")
+	}
+
+	updateMaxDLRL(tree.schemaMap, 0, 0)
 
 	var schemaElements []*parquet.SchemaElement
-	err = toParquetSchema(tree, "", "", &schemaElements, &valueElements)
-	if err != nil {
+	if err = toParquetSchema(tree, "", "", &schemaElements, &valueElements); err != nil {
 		return nil, nil, err
 	}
+
+	tree.readOnly = true
 
 	numChildren := int32(len(tree.keys))
 	schemaList = append(schemaList, &parquet.SchemaElement{
